86a61c8d43ccb9095ce231fb117262538a0e10f4
Guilherme Magalhães <107112139+Strikkerson@users.noreply.github.com>
2024-09-10 22:01:35 -0400

    Update AccountRegister.java

--- src/accountregister/AccountRegister.java
+++ src/accountregister/AccountRegister.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package accountregister;
 
-/**
- *
- * @author Strikke
- */
-public class AccountRegister {
 
-    /**
-     * @param args the command line arguments
-     */
+public class AccountRegister {
     public static void main(String[] args) {
         CreateTextFile textFileCreator = new CreateTextFile();
         textFileCreator.openFile();
